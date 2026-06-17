e66d4d6a7ac76a2279b3b4472ffc04c1e03f27f2
NouveauReece <nouveaureece@protonmail.com>
2026-06-17 15:52:57 -0400

    adding checks

---
+++ code/src/bookmarklet/checks/README.md
@@ -0,0 +1,7 @@
+# Checks
+
+## License
+These checks come from `axe-core/lib/checks` commit `f0a12cf` (12 June 2026). All files in this directory are used under Mozilla Public License 2.0 (MPL-2.0), dequelabs. 
+
+## Updating
+This directory was directly copied and is not managed by `npm`. To update it, the files will need to be replaced.
--- code/src/bookmarklet/rules/types.ts
+++ code/src/bookmarklet/rules/types.ts
@@ -1,10 +1,14 @@
+import type { AxeResults, ElementContext } from 'axe-core'
+
 export interface Rule {
   title: string;
   description: string;
   friendlyDescription: string;
   impact?: 'critical' | 'serious' | 'moderate' | 'minor';
   howToFix?: string;
+  customCheck?: CustomCheckFunction;
   autoFixFunction?: AutoFixFunction;
 }
 
+export type CustomCheckFunction = (node: ElementContext) => Promise<AxeResults>;
 export type AutoFixFunction = (root: HTMLElement) => void;
